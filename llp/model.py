2dd6247d0d593ef25585747aa4f330dba21fc736
showmeyourfeet <showyoumyfeet@gmail.com>
2025-12-24 15:32:01 +0800

    re-organize the file distribution for the recover mode.

--- llp/model.py
+++ llp/model.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
 
-from .DETRTransformer import (
+from additional_modules.DETRTransformer import (
     TransformerEncoder,
     TransformerEncoderLayer,
     build_transformer,
